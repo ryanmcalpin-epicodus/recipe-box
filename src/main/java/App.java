6abcb5e0ab324299040559eec32f44d6775cb5b6
Ryan <rhmcalpin@gmail.com>
2017-04-06 16:44:27 -0700

    fix a route, still incomplete

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -91,9 +91,33 @@ public class App {
       Recipe recipe = Recipe.find(Integer.parseInt(request.params(":id")));
       Ingredient ingredient = Ingredient.find(Integer.parseInt(request.params(":ingredient_id")));
       recipe.addIngredient(ingredient);
+      System.out.println(recipe.getIngredients());
       model.put("recipe", recipe);
       model.put("ingredients", recipe.getIngredients());
       model.put("template", "templates/recipe.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/ingredients/:id/recipes", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Ingredient ingredient = Ingredient.find(Integer.parseInt(request.params(":id")));
+      model.put("recipes", ingredient.getRecipes());
+      model.put("ingredient", ingredient);
+      model.put("template", "templates/ingredient-recipes.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/recipes/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Recipe recipe = Recipe.find(Integer.parseInt(request.params(":id")));
+      model.put("recipe", recipe);
+      model.put("template", "templates/recipe.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/categories", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/categories.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
