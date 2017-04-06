4c403b9ec8cc2c38d3a7675e85f3531ced9dcd75
Ryan <rhmcalpin@gmail.com>
2017-04-06 15:45:49 -0700

    add more routing, unfinished and broken

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -22,5 +22,80 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/ingredients", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("ingredients", Ingredient.all());
+      model.put("template", "templates/ingredients.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
+    get("/categories", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("categories", Category.all());
+      model.put("template", "templates/categories.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/recipes/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/recipe-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/recipes", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Recipe newRecipe = new Recipe(request.queryParams("name"), request.queryParams("instructions"));
+      newRecipe.save();
+      model.put("recipe", newRecipe);
+      model.put("template", "templates/recipe.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/recipes/:id/ingredients", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Recipe recipe = Recipe.find(Integer.parseInt(request.params(":id")));
+      model.put("recipe", recipe);
+      model.put("ingredients", Ingredient.all());
+      model.put("template", "templates/recipe-ingredients-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/recipes/:id/ingredients/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Recipe recipe = Recipe.find(Integer.parseInt(request.params(":id")));
+      model.put("recipe", recipe);
+      model.put("template", "templates/ingredient-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/ingredients/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/ingredient-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/ingredients", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Ingredient newIngredient = new Ingredient(request.queryParams("name"));
+      newIngredient.save();
+      Recipe recipe = Recipe.find(Integer.parseInt(request.queryParams("recipeId")));
+      model.put("ingredients", Ingredient.all());
+      model.put("recipe", recipe);
+      model.put("template", "templates/recipe-ingredients-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/recipes/:id/ingredients/:ingredient_id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Recipe recipe = Recipe.find(Integer.parseInt(request.params(":id")));
+      Ingredient ingredient = Ingredient.find(Integer.parseInt(request.params(":ingredient_id")));
+      recipe.addIngredient(ingredient);
+      model.put("recipe", recipe);
+      model.put("ingredients", recipe.getIngredients());
+      model.put("template", "templates/recipe.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
   }
 }
